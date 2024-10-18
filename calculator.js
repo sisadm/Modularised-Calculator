83ff42085cdd4be90d0f126adaa417f5bc18252b
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-10-18 21:45:30 +0100

    wrapping a and b to Number() if the passed arguments are strings

--- calculator.js
+++ calculator.js
@@ -1,10 +1,10 @@
 
-let add = (a, b) => a + b;
+let add = (a, b) => Number(a) + Number(b);
 
-let subtract = (a, b) => a - b;
+let subtract = (a, b) => Number(a) - Number(b);
 
-let multiply = (a, b) => a * b;
+let multiply = (a, b) => Number(a) * Number(b);
 
-let divide = (a, b) => a / b;
+let divide = (a, b) => Number(a) / Number(b);
 
 export {add, subtract, multiply, divide}
