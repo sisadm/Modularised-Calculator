2c6276ca6ed6c29d0cf1bb0b12a0f2734d7465bf
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-10-18 21:43:54 +0100

    creating an if statement to check the second character

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ import {add, subtract, multiply, divide} from "./calculator.js";
 
 const message = process.argv[2];
 
-let msg = message.split('');
+let msgArr = message.split('');
 
 function removeWhiteSpace(arr) { return arr.filter((item) => item != ' ')};
 
-msg = removeWhiteSpace(msg);
+msgArr = removeWhiteSpace(msgArr);
 
-console.log(msg)
+if(msgArr[1] == '+') {
+	console.log(`${msgArr[0]} + ${msgArr[2]} = ${add(msgArr[0], msgArr[2])}`);
+}
+
+
+
+
