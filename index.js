e0be1de6024be4ac1f8fad869bc49808465f8d61
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-10-20 15:25:59 +0100

    exporting the separeted message to the utils.cjs

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 import {add, subtract, multiply, divide} from "./esm/calculator.mjs";
 
-
+const readFile = import("./commonjs/utils.cjs")
 
 const message = process.argv[2];
 
 let msgArr = message.split('');
+
+
+function msgSend()  {  return `${msgArr[0]} ${msgArr[1]} ${msgArr[2]} = ${add(msgArr[0], msgArr[2])}`}
 
 function removeWhiteSpace(arr) { return arr.filter((item) => item != ' ')};
 
 msgArr = removeWhiteSpace(msgArr);
 
 if(msgArr[1] == '+') {
-	console.log(`${msgArr[0]} + ${msgArr[2]} = ${add(msgArr[0], msgArr[2])}`);
+	console.log(`${msgArr[0]} ${msgArr[1]} ${msgArr[2]} = ${add(msgArr[0], msgArr[2])}`);
 }
 
 
+readFile;
 
 
+export { msgSend } 
