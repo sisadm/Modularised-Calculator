dfaeb0d14d0e06e2664781b34daccccb81ba2550
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-10-20 15:32:20 +0100

    import msg function, read function using msgSend() plus writeFileSync()

--- commonjs/utils.cjs
+++ commonjs/utils.cjs
@@ -1,8 +1,68 @@
-const fs = require("fs");
+const msg = import("./../index.js")
+
+const fs = require("node:fs");
 const path = require("path");
 
-const filePath = path.join(_dirname, "message.json");
+const filePath = path.join(__dirname, "..", "messages.json");
 
-module.exports = {
-	readFile: (file) => fs.readFile
+
+const read = () => {
+	
+	try {
+		// read the content of the file
+		const data = fs.readFileSync(filePath, 'utf8');
+
+
+		// convert Json to object
+		const obj = JSON.parse(data);
+
+		let indexMsg;
+
+
+		// using the imported Promise from index.js 
+		msg.then((value) => {
+			indexMsg = value.msgSend();
+			console.log('inside', indexMsg)
+
+			// push message to the object
+			obj.push(indexMsg)
+
+			// convert object bac to JSON so you can weite it back to the file 	
+			const json = JSON.stringify(obj, null, 2);
+			write(filePath, json);
+
+		console.log(json)
+		}).catch((error) => {
+			console.error(error);
+		})	
+
+	} catch (err) {
+		console.error(err);
+	}	
+};
+
+const write = (path, json) => {
+
+	try {
+		fs.writeFileSync(path, json);
+		console.log("Message written to message.txt successfully!"); 
+	} catch (err) {
+		console.error(err);
+	}
 }
+
+
+
+
+  // readFile: () => {fs.readFile(filePath, 'utf8', (err, data) => {
+  // 	if (err) {
+  //   	console.error("Error reading the file:", err);
+  //   	return;
+  // 	}
+  // 	console.log(data);
+  // 	});
+// 	}
+
+
+module.exports = read();
+
